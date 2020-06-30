1bb5aa5ee970da5d8bd1a442051bd16af5a8e987
Wim Verhoogt <wim@verbant.nl>
2020-06-30 09:57:04 +0200

    Vertalingen

---
+++ lang/en/lang.php
@@ -0,0 +1,13 @@
+<?php return [
+    'beschikbaarheid' => [
+        'h2' => 'Availability',
+    ],
+    'hoofdmenu' => [
+        'welkom' => 'Welcome',
+        'kalender' => 'Calendar',
+        'over_ons' => 'About us',
+        'gastenboek'  => 'Guestbook',
+        'haarlem_e_o' => 'Haarlem',
+        'contact' => 'Contact'
+    ]
+];
--- lang/nl/lang.php
+++ lang/nl/lang.php
@@ -2,4 +2,12 @@
     'beschikbaarheid' => [
         'h2' => 'Beschikbaarheid',
     ],
+    'hoofdmenu' => [
+        'welkom' => 'Welkom',
+        'kalender' => 'Kalender',
+        'over_ons' => 'Over ons',
+        'gastenboek'  => 'Gastenboek',
+        'haarlem_e_o' => 'Haarlem e.o.',
+        'contact' => 'Contact'
+    ]
 ];
